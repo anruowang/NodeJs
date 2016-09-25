fd11cc54ee33e92a739a2d1faec034d57c9b2f06
anruowang <1137211995@qq.com>
2016-09-25 23:22:24 +0800

    goods

---
+++ lx9.23node/goods1/page/js/index.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Administrator on 2016/9/22.
+ */
+//打开登录窗口
+function showLogin(){
+	$("#uname").val("");
+	$("#pwd").val("");
+	$("#loginpages").mywin({left:"center",top:"center"});
+	$("#zcpages").hide();
+	$(".bg").fadeIn("200","linear");
+}
+
+//关闭层
+function hidenloginpage(){
+	$("#loginpages").hide();
+	$(".bg").fadeOut();
+}
+
+//打开注册窗口
+function showRegister(){
+	$("#zcuname").val("");
+	$("#zcpwd").val("");
+	$("#zcpwdagain").val("");
+	$("#zcpages").mywin({left:"center",top:"center"});
+	$("#loginpages").hide();
+	$(".bg").fadeIn("200","linear");
+	$("#registertishi").html("");
+}
+
+//关闭注册窗口
+function hidenzcpage(){
+	$("#zcpages").hide();
+	$(".bg").fadeOut();
+}
+
+//用户注册
+function userzc(){
+	var uname=$trim($("#zcuname").val());
+	var pwd=$trim($("#zcpwd").val());
+	var pwdagain=$trim($("#zcpwdagain").val());
+
+	$.post("userRegister",{uname:uname,pwd:pwd,pwdagain:pwdagain},function(data){
+		data= $.trim(data);
+		switch (data){
+			case "1":$("#registertishi").text("用户名不能为空....");break;
+			case "2":$("#registertishi").text("密码不能为空....");break;
+			case "3":$("#registertishi").text("两次密码输入不一致....");break;
+			case "4":$("#registertishi").text("数据库连接失败....");break;
+			case "5":$("#registertishi").text("数据添加失败....");break;
+			case "6":$("#registertishi").text("注册成功....");hidenzcpage();break;
+			default:$("#registertishi").text("注册失败。。。。");break;
+		}
+	},"text");
+}
---
+++ lx9.23node/goods1/server.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Administrator on 2016/9/22.
+ */
+var express=require("express");//创建服务器的
+var cookieparser=require("cookie-parser");//cookie
+var session=require("express-session");//session
+var mysql=require("mysql");//数据库
+var fs=require("fs");//操作文件或目录
+var bodyparser=require("body-parser");//处理请求的
+var multer=require("multer");//处理文件上传的
+
+var app=express();//创建一个应用程序
+
+//使用中间件
+app.use(express.static("page"));//默认到page文件夹查找静态资源
+
+app.use(bodyparser.urlencoded({extended:false}));
+
+//配置和使用session中间件
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: false,maxAge:1000*60*2 }
+}));
+
+//配置文件上传的中间件
+var upload=multer({dest:"./page/pic"});//上传文件的目录摄制
+
+//配置数据库连接池
+var pool=mysql.createPool({
+	host:"127.0.0.1",
+	port:3306,
+	database:"goods",
+	user:"root",
+	password:"1"
+});
+
+//处理用户注册的方法
+app.post("/userRegister",function(req,res){
+	var result="0";
+	if(req.body.uname==""){
+		res.send("1");//说明用户名为空
+	}else if(req.body.pwd==""){
+		res.send("2")
+	}else if(req.body.pwd!=req.body.pwdagain){
+		res.send("3");//说明两次密码不一致
+	}else {//访问数据库
+		pool.getConnection(function (err, connection) {
+			if (err) {
+				res.send("4");//说明数据库连接失败
+			} else {
+				connection.query("insert into adminInfo values(0,?,?)", [req.body.uname, req.body.pwd], function (err, result) {
+					if (err) {
+						res.send("5");//说明添加数据失败
+					} else {
+						res.send("6");//注册成功
+					}
+				});
+			}
+		});
+	}
+});
+
+app.listen(8088,function(err){
+	if(err){
+		console.info(err);
+	}else{
+		console.info("应用程序的启动成功...");
+	}
+});
